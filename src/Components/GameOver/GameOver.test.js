8d3eb3fe2228710584beb4a486d34b699c352b9f
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-27 16:59:38 -0600

    :rotating_light: Fix linter warnings

--- src/Components/GameOver/GameOver.test.js
+++ src/Components/GameOver/GameOver.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GameOver from './GameOver';
-
-import { MemoryRouter } from 'react-router-dom';
-import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('GameOver', () => {
--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-
-import { MemoryRouter } from 'react-router-dom';
-import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Home', () => {
