2fd47d9de288b4fb67d60f8a73ce3f5d8fb85c98
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-27 14:16:25 -0600

    :sparkles: Add basic elements to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.scss';
 
 const Home = () => {
   return (
-    <p>Home</p>
+    <section className='home-container'>
+      <h1 className='home-header'>Tandem for 400</h1>
+      <p className='home-info'>Train your trivia knowledge!</p>
+      <Link to='/play'>Play</Link>
+    </section>
   )
 }
 
