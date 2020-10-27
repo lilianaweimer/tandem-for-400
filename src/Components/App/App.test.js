52c2f4fa415976d09cb6357e2e42aa02426658de
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-27 15:57:38 -0600

    :white_check_mark: Add passing unit test to App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('App', () => {
+
+  it('should the home component on initial mount', () => {
+    const { getByText, getByRole } = render (
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(getByText('Tandem for 400')).toBeInTheDocument();
+    expect(getByText('Train your trivia knowledge!')).toBeInTheDocument();
+    expect(getByRole('link', {name: 'Play'})).toBeInTheDocument();
+  });
+
+})
