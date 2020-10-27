d152acd11d4fe60578edd82427b989d52508e752
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-27 13:35:22 -0600

    :sparkles: Add hooks to add game data to App state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,18 @@
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
+import data from '../../Data/Apprentice_TandemFor400_Data.json';
+
 function App() {
+
+  const [gameData, updateGameData] = useState({});
+
+  useEffect(() => {
+    updateGameData(data);
+  }, [])
+
+  console.log(gameData)
+
   return (
     <div className="App">
 
