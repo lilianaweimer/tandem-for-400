1875fba1a4a0f01586c2d4a98862f93869c6eacd
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-28 13:56:40 -0600

    :white_check_mark: Add passing unit test to Home

--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,9 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+
+import { MemoryRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Home', () => {
 
-  it('should be true', async () => {
-    expect(true).toBe(true);
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it('should render correctly', async () => {
+    await act(async () => {
+      ReactDOM.render(<MemoryRouter><Home /></MemoryRouter>, container)
+    });
+
+    expect(container.querySelector('h1').textContent).toBe('Tandem for 400');
+    expect(container.querySelector('p').textContent).toBe('Train your trivia knowledge!');
+    expect(container.querySelector('[data-testid=play-button]').textContent).toBe('Play');
   });
 
 });
