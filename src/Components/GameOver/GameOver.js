1cb7cdd703ed855262653c756d9a48a5daf41188
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-29 14:04:38 -0600

    :sparkles: Change favicon and title

--- src/Components/GameOver/GameOver.js
+++ src/Components/GameOver/GameOver.js
@@ -5,6 +5,7 @@ import './GameOver.scss';
 const GameOver = ({ score, resetGame }) => {
   return (
     <section className='gameover-container'>
+      <img src='http://www.pngall.com/wp-content/uploads/2016/03/Light-Bulb-PNG-HD.png' alt='clipart of a lightbulb' className='lightbulb-img'/>
       <h2 className='gameover-header'>Game Over!</h2>
       <p className='gameover-score'>Your final score was {score} points.</p>
       <Link to='/play' onClick={() => resetGame()} className='new-game-button'>Play Again</Link>
