5b4045cf7606517169f13e7751cbfe4040d2812a
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-29 13:10:36 -0600

    :lipstick: Style Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,8 @@ import './Home.scss';
 const Home = () => {
   return (
     <section className='home-container'>
-      <h1 className='home-header'>Tandem for 400</h1>
+      <img src='http://www.pngall.com/wp-content/uploads/2016/03/Light-Bulb-PNG-HD.png' alt='clipart of a lightbulb' className='lightbulb-img'/>
+      <h1 className='home-header'>TANDEM FOR 400</h1>
       <p className='home-info'>Train your trivia knowledge!</p>
       <Link to='/play' data-testid='play-button'>Play</Link>
     </section>
